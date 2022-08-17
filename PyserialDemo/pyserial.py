a17ded20694f97681d43e601f27bb94d5d417a7e
wayne <ck10000978@gmail.com>
2022-08-17 20:01:01 +0800

    Fix loop

--- PyserialDemo/pyserial.py
+++ PyserialDemo/pyserial.py
@@ -8,18 +8,15 @@ angle = []
 def main():
     port = input("Please enter the port: ")
     ser = serial.Serial("COM3",115200,timeout=2)
-    while True:
-        if ser.in_waiting >= 0:
-            b = ser.readline().decode("utf-8")
-            print(b)
-            angle.append(b)
-            plt.plot(angle,'o')
-        elif keyboard.is_pressed("q"):      
-            plt.show() 
-            break       
+    flag = False
 
-
-
+    while not(flag):
+        b = ser.readline().decode("utf-8")
+        print(b)
+        angle.append(b)
+        plt.plot(angle,'o')
+        flag = keyboard.is_pressed("q")
+    plt.show() 
 
 if __name__ == "__main__":    
         main()  
