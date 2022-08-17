643be81675ec7206bf6ceb38681dc5361a1102da
wayne <ck10000978@gmail.com>
2022-08-17 20:07:58 +0800

    add plt func

--- PyserialDemo/pyserial.py
+++ PyserialDemo/pyserial.py
@@ -10,13 +10,16 @@ def main():
     ser = serial.Serial("COM3",115200,timeout=2)
     flag = False
 
+    plt.ion()
     while not(flag):
         b = ser.readline().decode("utf-8")
         print(b)
         angle.append(b)
         plt.plot(angle,'o')
+        plt.pause(0.001)
+        plt.ioff() # close window
         flag = keyboard.is_pressed("q")
-    plt.show() 
+
 
 if __name__ == "__main__":    
         main()  
